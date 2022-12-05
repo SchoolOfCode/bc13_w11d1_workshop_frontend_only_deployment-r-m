915ca235724ca825435eafd07aa7706b14b4c4cb
MarwaDawood <marwa.dawood@hotmail.com>
2022-12-05 15:03:25 +0000

    changes

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+<h1 className='title'> Hello and Welcome to our App </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
